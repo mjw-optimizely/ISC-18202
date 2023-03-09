accd714030384a836baceca4f78018c4b17117cb
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-03-09 15:07:07 -0700

    It looks like we have to adopt their iframe

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,48 @@ router.get("/", function (req, res, next) {
     res.render("index", { title: "Stripe" });
 });
 
-router.post("/authenticated", async function (req, res, next) {
-    res.render("result", {
-        status: "Good",
-        data: `Received: ${req.body.skey}, ${req.body.pan}, ${req.body.expm}, ${req.body.expy}, ${req.body.cvv}, ${req.body.radarSessionId}`,
+function step1_createPaymentIntent(amount) {
+    return new Promise((resolve, reject) => {
+        const params = new URLSearchParams();
+        params.append("amount", amount);
+        params.append("currency", "usd");
+        params.append("customer", "cus_NUlvJCIihFkt5H");
+        params.append("payment_method", "card_1MjqhDIentMRxMOauWJwy6Sw");
+        params.append("confirm", "true");
+
+        axios
+            .post(`${baseUrl}/v1/payment_intents`, params, {
+                headers: {
+                    Authorization: `Bearer ${secretKey}`,
+                },
+            })
+            .then(response => {
+                console.log(`BLAM! ${JSON.stringify(response.data)}`);
+                resolve({ secret: response.data.client_secret });
+            })
+            .catch(error => {
+                reject(error);
+            });
     });
+}
+
+router.post("/authenticated", async function (req, res, next) {
+    secretKey = req.body.skey;
+    console.log(
+        `Received: ${req.body.skey}, ${req.body.pan}, ${req.body.expm}, ${req.body.expy}, ${req.body.cvv}, ${req.body.amount}, ${req.body.radarSessionId}`,
+    );
+
+    try {
+        var paymentIntentResponse = await step1_createPaymentIntent(req.body.amount);
+
+        res.render("result", {
+            status: "Good",
+            data: "meow",
+            secret: paymentIntentResponse.secret,
+        });
+    } catch (error) {
+        res.render("error", { message: JSON.stringify(error) });
+    }
 });
 
 module.exports = router;
